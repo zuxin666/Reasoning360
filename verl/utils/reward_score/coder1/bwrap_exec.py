39455d77c2611235b38e033e4cbaeca4c155a5d0
samuel zhang <samuelzhang012@gmail.com>
2025-03-13 00:54:45 +0000

    restructured for passing in conda env

--- verl/utils/reward_score/coder1/bwrap_exec.py
+++ verl/utils/reward_score/coder1/bwrap_exec.py
@@ -7,7 +7,9 @@ from .utils import _ERROR_MSG_PREFIX, _DEFAULT_TIMEOUT_SECONDS
 
 CLI_ARG_SIZE_LIMIT = 1024 * 3
 
-def code_exec_bwrap(code, stdin: str = None, timeout=_DEFAULT_TIMEOUT_SECONDS, pytest: str = None):
+def code_exec_bwrap(code, stdin: str = None, timeout=5, 
+                    pytest: str = None, solution: str = None, 
+                    python_env: str = "/usr"):
     env = os.environ.copy()
     env["OPENBLAS_NUM_THREADS"] = "1"
     if "PYTHONPATH" in env:
@@ -17,14 +19,34 @@ def code_exec_bwrap(code, stdin: str = None, timeout=_DEFAULT_TIMEOUT_SECONDS, p
         "timeout", str(timeout),
         "bwrap",
         "--unshare-all",
-        "--ro-bind", "/usr/bin/python3", "/usr/bin/python3",
+        # "--ro-bind", "/usr/bin/python3", "/usr/bin/python3",
+        "--ro-bind", python_env, "/sandbox",
         "--ro-bind", "/usr/lib", "/usr/lib",
         "--ro-bind", "/usr/lib64", "/usr/lib64",
         "--ro-bind", "/lib", "/lib",
         "--ro-bind", "/lib64", "/lib64",
+        "--dev", "/dev",
+        "--proc", "/proc",
+        # "--new-session", # causes issues w. timeout
     ]
-
-    if pytest: # none of the reference solutions in code-r1-12k are pytest based
+    if solution: # not necessarily a pytest file. test solution is just in a separate file that imports solution
+        with TemporaryDirectory() as tmpdir:
+            assert stdin is None, "STDIN is not supported with solution_file"
+            with open(os.path.join(tmpdir, "solution.py"), "w") as f:
+                f.write(code)
+            with open(os.path.join(tmpdir, "test_solution.py"), "w") as f:
+                f.write(solution)
+            command.extend(["--ro-bind", tmpdir, tmpdir])
+            command.extend(["/sandbox/bin/python3", os.path.join(tmpdir, "test_solution.py")])
+            result = subprocess.run(
+                command,
+                cwd=tmpdir,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=env,
+                check=False,
+            )
+    elif pytest: # none of the reference solutions in code-r1-12k are pytest based
         with TemporaryDirectory() as tmpdir:
             assert stdin is None, "STDIN is not supported with pytest"
             with open(os.path.join(tmpdir, "solution.py"), "w") as f:
@@ -32,7 +54,7 @@ def code_exec_bwrap(code, stdin: str = None, timeout=_DEFAULT_TIMEOUT_SECONDS, p
             with open(os.path.join(tmpdir, "test_solution.py"), "w") as f:
                 f.write(pytest)
             command.extend(["--ro-bind", tmpdir, tmpdir])
-            command.extend(["python3", "-m", "pytest", tmpdir])
+            command.extend(["/sandbox/bin/python3", "-m", "pytest", tmpdir])
             result = subprocess.run(
                 command,
                 cwd=tmpdir,
@@ -43,7 +65,7 @@ def code_exec_bwrap(code, stdin: str = None, timeout=_DEFAULT_TIMEOUT_SECONDS, p
             )
     else:
         if len(code) < CLI_ARG_SIZE_LIMIT:
-            command.extend(["python3", "-c", code])
+            command.extend(["/sandbox/bin/python3", "-c", code])
             result = subprocess.run(command,
                                     input=stdin.encode() if stdin else None,
                                     stdout=subprocess.PIPE,
@@ -55,7 +77,7 @@ def code_exec_bwrap(code, stdin: str = None, timeout=_DEFAULT_TIMEOUT_SECONDS, p
                 tmp.write(code.encode())
                 tmp.flush()
                 command.extend(["--ro-bind", tmp.name, tmp.name])
-                command.extend(["python3", tmp.name])
+                command.extend(["/sandbox/bin/python3", tmp.name])
                 result = subprocess.run(command,
                                         input=stdin.encode() if stdin else None,
                                         stdout=subprocess.PIPE,
