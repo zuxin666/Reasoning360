7abf84c665c4515cf576658c3e003563da987add
Taylor Killian <twkillian@cs.toronto.edu>
2025-03-19 23:44:38 +0000

    syncing

--- scripts/templates/docker-fsdp-qwen7b-4nodes-ppo.sh
+++ scripts/templates/docker-fsdp-qwen7b-4nodes-ppo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=mbzuai
-#SBATCH --job-name=rl-taylor-test
+#SBATCH --job-name=taylor-ppo
 #SBATCH --nodes=4
 #SBATCH --ntasks=4
 #SBATCH --ntasks-per-node=1
@@ -51,11 +51,11 @@ BASE_MODEL=Qwen/Qwen2.5-7B-Instruct
 # BASE_MODEL=meta-llama/Meta-Llama-3-8B-Instruct
 
 # Parallel config
-SP_SIZE=1
-ROLLOUT_TP_SIZE=4
+TP_SIZE=2
+
 
 WANDB_PROJECT=Reasoning360
-WANDB_EXPERIMENT_NAME=taylor-7B-docker-microBatch16-math-${BASE_MODEL##*/}-${SLURM_JOB_ID}
+WANDB_EXPERIMENT_NAME=taylor-7B-docker-ppo_n16-math-${BASE_MODEL##*/}-${SLURM_JOB_ID}
 
 echo "Node list: ${nodes[@]}"
 
@@ -80,45 +80,47 @@ done
 
 
 cmd="python3 /Reasoning360/verl/trainer/main_ppo.py  --config-path=/Reasoning360/verl/trainer/config --config-name='ppo_trainer' \
-    algorithm.adv_estimator=grpo \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
     data.train_batch_size=128 \
-    data.val_batch_size=2048 \
     data.max_prompt_length=1024 \
     data.max_response_length=3072 \
+    data.filter_overlong_prompts=True \
+    data.val_batch_size=2048 \
     actor_rollout_ref.model.path=$BASE_MODEL \
-    actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.fsdp_size=-1 \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.1 \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.strategy="fsdp" \
-    actor_rollout_ref.actor.ulysses_sequence_parallel_size=${SP_SIZE} \
-    actor_rollout_ref.actor.use_kl_loss=True \
-    actor_rollout_ref.actor.kl_loss_coef=0.001 \
-    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=${ROLLOUT_TP_SIZE} \
-    actor_rollout_ref.rollout.n=64 \
-    actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16  \
+    actor_rollout_ref.rollout.max_num_seqs=256 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=${TP_SIZE} \
+    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
+    critic.optim.lr=1e-5 \
+    critic.model.enable_gradient_checkpointing=True \
+    critic.model.path=$BASE_MODEL \
+    critic.ppo_mini_batch_size=128 \
+    critic.ppo_micro_batch_size_per_gpu=16 \
+    critic.model.fsdp_config.param_offload=False \
+    critic.model.fsdp_config.optimizer_offload=False \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name=${WANDB_PROJECT} \
     trainer.experiment_name=${WANDB_EXPERIMENT_NAME} \
-    +trainer.val_before_train=True \
     trainer.n_gpus_per_node=8 \
+    +trainer.val_before_train=True \
     trainer.nnodes=$worker_num \
     trainer.save_freq=25 \
     trainer.test_freq=5 \
-    trainer.total_epochs=5"
+    trainer.total_epochs=10"
 
 node_i=${nodes[worker_num - 1]}
 echo "================================================"
