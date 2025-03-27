e0e0f6a3daca3ae99804333834c6e3f03f558759
Taylor Killian <twkillian@gmail.com>
2025-03-27 21:22:40 +0000

    Sync

--- scripts/dapo_updates/0326-conda-multinode-bigmath-qwen7b-4nodes-grpo.sh
+++ scripts/dapo_updates/0326-conda-multinode-bigmath-qwen7b-4nodes-grpo.sh
@@ -6,13 +6,13 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=96
-#SBATCH --mem=512G
+#SBATCH --mem=1T
 #SBATCH --output=slurm/verl-grpo-%j.out
 #SBATCH --error=slurm/verl-grpo-%j.err
 #SBATCH --exclusive
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 
-# sleep 172800
+# sleep 100
 #!/bin/bash
 
 # Get the list of allocated nodes
@@ -139,26 +139,26 @@ sleep 10
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.strategy="fsdp" \
     actor_rollout_ref.actor.ulysses_sequence_parallel_size=${SP_SIZE} \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.tensor_model_parallel_size=${ROLLOUT_TP_SIZE} \
     actor_rollout_ref.rollout.n=64 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name=${WANDB_PROJECT} \
     trainer.experiment_name=${WANDB_EXPERIMENT_NAME} \
-    +trainer.val_before_train=True \
+    +trainer.val_before_train=False \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=$worker_num \
     trainer.save_freq=10 \
