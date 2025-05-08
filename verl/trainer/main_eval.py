0e883457ae45bce10f7784c2fd99eed1bb16e3eb
Fan <koala99.zf@gmail.com>
2025-05-08 21:40:35 +0800

    more offline eval & multi-node support (#60)
    * add supergpqa
    
    * some fix
    
    * add aime25 & arcagi eval
    
    * fix
    
    * support multinode generation for offfline eval
    
    * refactor script structure

--- verl/trainer/main_eval.py
+++ verl/trainer/main_eval.py
@@ -19,31 +19,49 @@ The input is a parquet file that contains N generated sequences and (optional) t
 
 import hydra
 from verl.utils.fs import copy_to_local
-from verl.utils.reward_score import math, gsm8k, cruxeval, tablereason, naive_dapo, coder1, gpqa
+from verl.utils.reward_score import (
+    math,
+    gsm8k,
+    cruxeval,
+    tablereason,
+    naive_dapo,
+    coder1,
+    gpqa,
+    supergpqa,
+    arcagi,
+)
 import pandas as pd
 import numpy as np
 
 
 def select_reward_fn(data_source):
-    if data_source == 'lighteval/MATH':
+    if data_source == "lighteval/MATH":
         return math.compute_score
-    elif data_source.startswith('simulation__cruxeval'):
+    elif data_source.startswith("simulation__cruxeval"):
         return cruxeval.compute_score
-    elif data_source.startswith('table'):
+    elif data_source.startswith("table"):
         return tablereason.compute_score
     # math
-    elif data_source in ['aime_repeated_8x', 'math', 'math__olympiad_bench']:
+    elif data_source in ["aime_repeated_8x", "math", "olympiad_bench", "math__aime2025_repeated_8x"]:
         return naive_dapo.compute_score
     # code gen
-    elif data_source in ['codegen__humaneval', 'codegen__mbpp', 'codegen__livecodebench']:
+    elif data_source in [
+        "codegen__humaneval",
+        "codegen__mbpp",
+        "codegen__livecodebench",
+    ]:
         return coder1.compute_score
     elif data_source in ['stem__gpqa', 'stem__gpqa_diamond']:
         return gpqa.compute_score
+    elif data_source == "stem__supergpqa":
+        return supergpqa.compute_score
+    elif data_source == "simulation__arcagi1":
+        return arcagi.compute_score
     else:
-        raise NotImplementedError
+        raise NotImplementedError(f"Data source {data_source} not implemented")
 
 
-@hydra.main(config_path='config', config_name='evaluation', version_base=None)
+@hydra.main(config_path="config", config_name="evaluation", version_base=None)
 def main(config):
     local_path = copy_to_local(config.data.path)
     if 'livecodebench' in local_path:
@@ -55,7 +73,7 @@ def main(config):
     responses = dataset[config.data.response_key]
     data_sources = dataset[config.data.data_source_key]
     reward_model_data = dataset[config.data.reward_model_key]
-    extra_info_data = dataset['extra_info']
+    extra_info_data = dataset["extra_info"]
 
     passes = 0
 
@@ -68,7 +86,7 @@ def main(config):
         prompt = prompts[i]
         reward_data = reward_model_data[i]
         reward_fn = select_reward_fn(data_source)
-        ground_truth = reward_data['ground_truth']
+        ground_truth = reward_data["ground_truth"]
         extra_info = extra_info_data[i]
         score_lst = []
         for r in response_lst:
@@ -78,11 +96,11 @@ def main(config):
         max_score = np.max(score_lst)
         # print(f">>> {max_score}, {score_lst}")
 
-        if max_score['acc'] == 1:
+        if max_score["acc"] == 1:
             passes += 1
 
-    print(f'pass@1: {passes / total}')
+    print(f"pass@1: {passes / total}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
