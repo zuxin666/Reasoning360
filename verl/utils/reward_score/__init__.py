df78cc850fecc9a08ad00a83fad97ee6da55215e
Leolty <til040@ucsd.edu>
2025-05-15 01:07:07 +0000

    add supergpqa

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -64,6 +64,9 @@ def _default_compute_score(data_source, solution_str, ground_truth, extra_info=N
     elif data_source.startswith('stem__gpqa'):
         from . import gpqa
         res = gpqa.compute_score(solution_str, ground_truth)
+    elif data_source.startswith('stem__supergpqa'):
+        from . import supergpqa
+        res = supergpqa.compute_score(solution_str, ground_truth)
     elif data_source.startswith('stem_web'):
         from . import stem_llm_judge
         res = stem_llm_judge.compute_score(data_source=data_source, model_output=solution_str, ground_truth=ground_truth, extra_info=extra_info)
